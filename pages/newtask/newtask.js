7c0175b7ccee3af0ef8beba0280e345f648dfa83
Nemocdz <cdz1996@qq.com>
2016-12-17 01:15:57 +0800

    新增： 1.newtask页面css，html

--- pages/newtask/newtask.js
+++ pages/newtask/newtask.js
@@ -1,25 +1,26 @@
 Page({
   data:{
-      celltitle: "标题"
+      src:'../../icon/添加.png',
+      text: ''
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
-    String2
+    
   },
   onReady:function(){
     // 页面渲染完成
-    String3
+    
   },
   onShow:function(){
     // 页面显示
-    String4
+    
   },
   onHide:function(){
     // 页面隐藏
-    String5
+    
   },
   onUnload:function(){
     // 页面关闭
-    String6
+    
   }
 })
